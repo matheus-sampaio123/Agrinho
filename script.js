446a7cd699debf6b2591306590d879490d1b2145
matheus-sampaio123 <rodrigues.sampaio@escola.pr.gov.br>
2026-04-23 07:56:09 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
+// Saudação personalizada
+const btnSaudacao = document.getElementById("btn-saudacao");
+const saudacao = document.getElementById("saudacao");
 
+btnSaudacao.addEventListener("click", () => {
+  const nome = prompt("Qual é o seu nome?");
+  if (nome) {
+    saudacao.textContent = `Olá, ${nome}! Bem-vindo(a) ao Agrinho!`;
+  } else {
+    saudacao.textContent = "Olá! Bem-vindo(a) ao Agrinho!";
+  }
+});
+
+// Modo escuro
+const btnModo = document.getElementById("btn-modo");
+btnModo.addEventListener("click", () => {
+  document.body.classList.toggle("dark");
+});
