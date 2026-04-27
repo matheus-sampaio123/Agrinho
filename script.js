0437a5276a7666163784879233aa7fd960d204a9
matheus-sampaio123 <rodrigues.sampaio@escola.pr.gov.br>
2026-04-27 07:28:12 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,1 @@
-document.getElementById('darkModeToggle').addEventListener('click', function() {
-    document.body.classList.toggle('dark-mode');
-});
 
-// Adicionando o estilo para o modo escuro
-const style = document.createElement('style');
-style.innerHTML = `
-    .dark-mode {
-        background-color: #333;
-        color: white;
-    }
-    .dark-mode header, .dark-mode footer {
-        background-color: #444;
-    }
-`;
-document.head.appendChild(style);
