6c215327c6a923f699b073c6749947c22a132fb4
matheus-sampaio123 <rodrigues.sampaio@escola.pr.gov.br>
2026-04-27 13:39:05 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
-window.addEventListener('scroll', function() {
-    const header = document.querySelector('header');
-    // Adiciona classe 'sticky' ao rolar para baixo
-    if (window.scrollY > 50) {
-        header.style.background = 'rgba(0, 0, 0, 0.9)';
-        header.style.padding = '10px 50px';
-    } else {
-        header.style.background = 'rgba(0, 0, 0, 0.3)';
-        header.style.padding = '20px 50px';
-    }
-});
+function modoEscuro() {
+    document.body.classList.toggle("dark");
+}
 
-// Mensagem de boas-vindas no console (opcional)
-console.log("Portal Agro Sustentável carregado com sucesso.");
+function adicionarItem() {
+    let novo = prompt("Digite um novo benefício:");
+
+    if (novo) {
+        let li = document.createElement("li");
+        li.textContent = novo;
+
+        document.getElementById("lista").appendChild(li);
+    }
+}
